912b09042e6950ab9a5c660a04e8838e0baa9669
Nick <35193603+lunarTime@users.noreply.github.com>
2021-10-11 18:25:29 +0500

    Update indexphp.php

--- indexphp.php
+++ indexphp.php
@@ -6,7 +6,7 @@
     FILTER_SANITIZE_STRING);
 
     if(mb_strlen($login) < 5 || mb_strlen($login) > 90) {
-        echo "Недопустимая длина лsdfsdfsdогина";
+        echo "Недопустимая длина логина";
         exit();
     }
 ?>
